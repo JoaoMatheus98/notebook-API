ba46350a8473a9f3999022ca1cc46194d6090aed
JoaoMatheus98 <j.matheus.m98@gmail.com>
2022-03-23 16:11:50 -0300

    Desserializacao com AMS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    before_action :ensure_json_request
+
+    ## Obrigando a passar o accepted application/vnd.api+json na request, senão response em branco
+    def ensure_json_request
+        return if request.headers["Accept"]  =~ /vnd\.api\+json/
+        render :nothing => true, :status => 406
+    end
 end
