17cfbf2665f1fef2e70ef2a2765e4c63f0300a19
JoaoMatheus98 <j.matheus.m98@gmail.com>
2022-03-24 15:59:50 -0300

    Rotas JsonAPI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
     before_action :ensure_json_request
 
-    ## Obrigando a passar o accepted application/vnd.api+json na request, senão response em branco
+    ## Header => Accept: application/vnd.api+json . Else, blank error 406
     def ensure_json_request
         return if request.headers["Accept"]  =~ /vnd\.api\+json/
         render :nothing => true, :status => 406
