76832e7201748a48d30b812087df328a829576b2
JoaoMatheus98 <j.matheus.m98@gmail.com>
2022-04-05 10:45:33 -0300

    Http Basic

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,4 +1,9 @@
 class KindsController < ApplicationController
+
+  TOKEN = 'secret123'
+
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :authenticate
   before_action :set_kind, only: [:show, :update, :destroy]
 
   # GET /kinds
@@ -52,5 +57,13 @@ class KindsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def kind_params
       params.require(:kind).permit(:description)
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        # Compare the tokens in a time-constant manner, to mitigate
+        # timing attacks.
+        ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+      end
     end
 end
